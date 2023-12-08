66b7cd09370410db0f9a2739a305af957ebc20f6
cr2551 <92417699+cr2551@users.noreply.github.com>
2023-12-07 23:02:13 -0600

    day5 part1 solved

--- day5/seeds.py
+++ day5/seeds.py
@@ -2,8 +2,8 @@ import sys
 import re
 import os
 script_path = os.path.dirname(os.path.abspath(__file__))
-# rel_file_path = 'input'
-rel_file_path = 'example'
+rel_file_path = 'input'
+# rel_file_path = 'example'
 file_path = os.path.join(script_path, rel_file_path)
 
 file_desc = open(file_path)
@@ -14,43 +14,49 @@ file = file_desc.read().strip()
 # split text input by empty line
 pattern = r"\n\n"
 categories = re.split(pattern, file)
+
+# parse text to create seeds array
 seeds = categories[0].split(':')[1]
 seeds = [int(x) for x in seeds.split()]
 
-seed_to_soil_map = [x for x in categories if x.startswith('seed-to-soil')][0]
-seed_to_soil = seed_to_soil_map.split(':')[1].split('\n')[1:]
 
 categories = categories[1:]
 # i think i will do depth-first search
 
+
+# convert categories into a 3d array that we can follow
 for i in range(len(categories)):
     categories[i] = categories[i].split(':\n')[1]
     # categories[i] = [int(x) for x in categories[i].split('\n')]
-    categories[i] = [x for x in categories[i].split('\n')]
+    categories[i] = [cat for cat in categories[i].split('\n')]
     for j in range(len(categories[i])):
-        categories[i][j] = [cat for cat in categories[i][j].split()]
-        
+        categories[i][j] = [int(x) for x in categories[i][j].split()]
 
+locations = []
+# now search
+for seed in seeds:
+    source  = seed
+    for i in range(len(categories)):
+        matched = False
+        for j in range(len(categories[i])):
+            # destination range start
+            dest_start = categories[i][j][0]
+            # source range start
+            source_start = categories[i][j][1]
+            # the length of the range
+            range_length = categories[i][j][2]
+            if source >= source_start and (source_start + range_length) >= source:
+                matched = True
+                dest_id  = (source - source_start) + dest_start
+                source = dest_id
+                break
+    locations.append(dest_id)
+    # print(seed, dest_id)
 
-for i in range(len(seed_to_soil)):
-    if seed_to_soil[i]: 
-        seed_to_soil[i] = [int(x) for x in seed_to_soil[i].split()]
-        
-        # identify which maps have a range where our seeds fall on
-        destination = seed_to_soil[i][0]
-        source = seed_to_soil[i][1]
-        range_length = seed_to_soil[i][2]
-        for seed in seeds:
-            if (source <= seed) and ((source + range_length) >= seed):
-                # print(seed, seed_to_soil[i])
-                soil_id = (seed - source) + destination
-                # print(seed, soil_id) 
-
-
-    
-# print(lines)
 file_desc.close()
-print(categories)
+# print(categories)
+# print(locations)
+print(min(locations))
+
 # print(seeds)
-# print(seed_to_soil)
 
